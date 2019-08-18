bfca24f30bdaa25a0602bff088757dbc2bce6288
Krzysztof Atlasik <krzysztof.atlasik@protonmail.com>
2019-08-18 15:00:50 +0200

    Fixed typo

--- classes/src/main/java/pl/sda/modify/ModifyExercise.java
+++ classes/src/main/java/pl/sda/modify/ModifyExercise.java
@@ -3,17 +3,38 @@ package pl.sda.modify;
 import java.util.List;
 public class ModifyExercise {
 
+    /**
+     * Stwórz metodę modifyUpperCase, która będzie otrzymywać listę stringów oraz liczbę (indeks elementu).
+     * Gdy indeks nie będzie wskazywał na żaden element rzuć IllegalArgumentException.
+     * W przypadku gdy zostanie podany poprawny indeks to zmień wszystkie znaki
+     * we wskazanym stringu na wielkie litery, a resztę pozostaw niezmienioną.
+     * Na przykład: ["aaa", "bbb", "ccc", "ddd"] oraz 2 -> ["aaa", "bbb", "CCC", "ddd"]
+     */
     public static List<String> modifyUppercase(List<String> strings, int index) {
         return null;
     }
 
+    /**
+     *
+     * Stwórz analogiczną metody modifyRepeat, która powiela string dwukrotnie.
+     * ["aaa", "bbb", "ccc", "ddd"] oraz 2 -> ["aaa", "bbb", "cccccc", "ddd"]
+     *
+     */
     public static List<String> modifyRepeat(List<String> strings, int index) {
         return null;
     }
 
+    /**
+     * Zastanów się, czy za pomocą interfejsów można stworzyć ogólną wersję metody modify.
+     */
+
     public static List<String> modify(List<String> strings, int index) {
         return null;
     }
+
+    /**
+     * Zastanów się, czy nie można zmienić metody modify, tak by można było wyszukiwać element po dowolnej właściwości nie tylko po indeksie.
+     */
 
     public static List<String> modifyFind(List<String> strings) {
         return null;
