22cf8289e1225cfa647ee1890835369e1bdd36ae
Krzysztof Atlasik <krzysztof.atlasik@protonmail.com>
2019-11-17 17:16:27 +0100

    Fixes

--- enums/src/main/java/pl/sda/enums/Color.java
+++ enums/src/main/java/pl/sda/enums/Color.java
@@ -2,7 +2,7 @@ package pl.sda.enums;
 
 /**
  * Stwórz typ wyliczeniowy `Color`.
- * Dodaj kolory, takie jak `BLACK`, `RER`, `YELLOW`.
+ * Dodaj kolory, takie jak `BLACK`, `RED`, `YELLOW`.
  * Dodaj do niego metode `String getHex()` zwracającą heksadecymalną reprezentację koloru.
  */
 
--- generics/src/main/java/pl/sda/generics/AllAndAny.java
+++ generics/src/main/java/pl/sda/generics/AllAndAny.java
@@ -8,7 +8,6 @@ import java.util.function.Predicate;
 public class AllAndAny {
 
     static <T> boolean all(Collection<T> collection, Predicate<T> predicate) {
-
         return true;
     }
 
@@ -18,13 +17,15 @@ public class AllAndAny {
 
     public static void main(String[] args) {
 
-        System.out.println(all(Set.of("raz", "dwa", "trzy"), s -> s.length() >= 2));
-        System.out.println(all(List.of(5, 3, 1, 27), i -> i % 2 != 0));
+        System.out.println(all(Set.of("raz", "dwa", "trzy"), s -> s.length() >= 2)); //true
+        System.out.println(all(Set.of("raz", "d", "trzy"), s -> s.length() >= 2)); //false
+        System.out.println(all(List.of(5, 3, 1, 27), i -> i % 2 != 0)); //true
+        System.out.println(all(List.of(1, 8, 5), i -> i % 2 != 0)); //false
 
-
-        System.out.println(any(List.of(5, 4, 1, 27), i -> i % 2 == 0));
-
-        System.out.println(any(List.of("raz", "dwa", "trzy"), s -> s.equals("dwa`")));
+        System.out.println(any(List.of(5, 4, 1, 27), i -> i % 2 == 0)); //true
+        System.out.println(any(List.of(5, 1, 27), i -> i % 2 == 0)); //false
+        System.out.println(any(Set.of("raz", "dwa", "trzy"), s -> s.equals("dwa")));//true
+        System.out.println(any(Set.of("raz", "trzy"), s -> s.equals("dwa"))); //false
 
     }
 
